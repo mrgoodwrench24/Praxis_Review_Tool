3fe30d21478cf15a6260d6abc2d6cae84b5ec237
michaeltroywhite24 <michaeltroywhite24@gmail.com>
2022-09-20 17:35:04 -0600

    Initial commit

---
+++ src/Controller/MainViewController.java
@@ -0,0 +1,60 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+
+public class MainViewController {
+    @FXML
+    private ToggleGroup checkGroup;
+
+    @FXML
+    private TextField initialsTxt;
+
+    @FXML
+    private TextField praxisNoteTxt;
+
+    @FXML
+    private ComboBox<?> statesComboBox;
+
+    @FXML
+    private HBox webView;
+
+    @FXML
+    void onActionClear(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionCopy(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionFirst(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionHome(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionPrograms(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionSecond(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionStates(ActionEvent event) {
+
+    }
+}
